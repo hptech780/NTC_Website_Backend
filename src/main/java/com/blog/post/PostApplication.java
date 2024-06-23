3bbd259c6e6b98d5682a138ded9f1af6889d19eb
hptech780 <httech780@gamil.com>
2024-06-23 19:11:16 +0545

    enabled cors

--- src/main/java/com/blog/post/PostApplication.java
+++ src/main/java/com/blog/post/PostApplication.java
@@ -20,7 +20,7 @@ public class PostApplication {
 	            public void addCorsMappings(CorsRegistry registry) {
 	                registry.addMapping("/**")
 	                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
-	                        .allowedOrigins("http://localhost:5173")
+	                        .allowedOrigins("http://localhost:5173","https://ntc-website-frontend.onrender.com")
 	                        .allowedHeaders("*");
 	            }
 	        };
